bc5123025dd28985c480d01c88b4e62e5f6261b1
Mike15234 <miguelantonio1852@gmail.com>
2020-02-19 18:42:55 -0500

    codigo arbol modificado

---
+++ Laboratorio2/BEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio2
+{
+    public class BEntry<TKey, T> : IEquatable<BEntry<TKey, T>>, IEntry<TKey, T>
+    {
+        public TKey LLave { get; set; }
+
+        public T Apuntador { get; set; }
+        
+        public bool Equals(BEntry<TKey, T> other)
+        {
+            return this.LLave.Equals(other.LLave) && this.Apuntador.Equals(other.Apuntador);
+        }
+
+    }
+    public interface IEntry<TKey, T>
+    {
+        bool Equals(BEntry<TKey, T> other);
+    }
+}
---
+++ Laboratorio2/NodoAsterisco.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio2
+{
+    public class NodoAsteriscoo<TKey, T>
+    {
+
+            private int grado;
+        
+            public NodoAsteriscoo(int grado)
+            {
+                this.grado = grado;
+                this.Hijos = new List<NodoAsteriscoo<TKey, T>>(grado + 1);
+                this.Entradas = new List<BEntry<TKey, T>>(grado);
+            }
+
+            public List<NodoAsteriscoo<TKey, T>> Hijos { get; set; }
+
+            public List<BEntry<TKey, T>> Entradas { get; set; }
+
+            public bool EsHoja
+            {
+                get
+                {
+                    return this.Hijos.Count == 0;
+                }
+            }
+
+            public bool AlcanzaMaximaEntrada
+            {
+                get
+                {
+                    return this.Entradas.Count == (this.grado);
+                }
+            }
+
+        public bool AlcanzaMaximaEntradaRaiz
+        {
+            get
+            {
+                return this.Entradas.Count == ((4/3)*(this.grado-1));
+            }
+        }
+
+        public bool AlcanzaMinimaEntrada
+            {
+                get
+                {
+                    return this.Entradas.Count == ((2/3)*(this.grado - 1));
+                }
+            }
+        }
+
+    
+}
